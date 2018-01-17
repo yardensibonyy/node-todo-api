131412eeb163ab79dbc910fc0b02e2b755b3831c
Yarden Sibony <yardensibonyy@gmail.com>
2018-01-17 16:37:05 +0200

    Add test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -36,10 +36,11 @@ app.get('/todos/:id', (req, res) => {
         return res.status(404).send();
     }
     Todo.findById(id).then((todo) => {
-        if (todo) {
-           return res.status(200).send({todo: todo}); 
+        if (!todo) {
+           return res.status(404).send(); 
         }
-        res.status(404).send();
+        res.status(200).send({todo});
+        
     }).catch((e) => res.status(400).send());
 });
 
