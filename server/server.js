56a922727170824eee0ded766aa7800d3c1c8274
Yarden Sibony <yardensibonyy@gmail.com>
2018-01-17 14:21:39 +0200

    Add tests for GET /todos

--- server/server.js
+++ server/server.js
@@ -22,8 +22,8 @@ app.post('/todos', (req, res) => {
 });
 
 app.get('/todos', (req, res) => {
-    Todo.find().then((doc) => {
-        res.send({doc});
+    Todo.find().then((todos) => {
+        res.send({todos});
     }, (e) => {
         res.status(400).send(e);
     });
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,11 +4,19 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+    text: 'first test todo'
+}, {
+    text: 'second test todo'
+}];
+
 beforeEach((done) => {
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
 });
 
-describe('Post /todos', () => {
+describe('POST /todos', () => {
     it('should create a new todo', (done) => {
         let text = 'test to do text';
 
@@ -24,7 +32,7 @@ describe('Post /todos', () => {
                     return done(err); //return stops the function execution
                 }
 
-                Todo.find().then((todos) => {
+                Todo.find({text}).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -44,9 +52,21 @@ describe('Post /todos', () => {
                 }
                 
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((e) => done(e));
             });
+    });
+});
+
+describe('GET /todos', () => {
+    it('should get all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done);
     });
 });
