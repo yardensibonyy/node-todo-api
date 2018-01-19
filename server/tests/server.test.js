1a66198a517e3f5b7e917d755eb787ecd91c954e
Yarden Sibony <yardensibonyy@gmail.com>
2018-01-20 01:42:38 +0200

    Test the DELETE/todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -100,3 +100,38 @@ describe('GET /todos/:id', () => {
     });
 });
 
+describe('DELETE /todos/:id', () => {
+    it ('should remove a todo', (done) => {
+        let hexId = todos[0]._id.toHexString();
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo._id).toBe(hexId);
+            })
+            .end((err, res) => {
+                if(err) {
+                    return done(err);
+                }
+                Todo.findById(hexId).then((todo) => {
+                    expect(todo).toBe(null); //equal to 'toNotExist()'
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+
+    it ('should return 404 if todo not found', (done) => {
+        var hexIdTest = new ObjectID().toHexString(); //creating a random valid id
+        request(app)
+            .delete(`/todos/${hexIdTest}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it ('should return 404 if objectId is not valid', (done) => {
+        request(app)
+            .delete('/todos/123')
+            .expect(404)
+            .end(done);
+    });
+});
