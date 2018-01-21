f4ee42b3f34adeaaabc740f6b90c5f2da0e622a6
Yarden Sibony <yardensibonyy@gmail.com>
2018-01-21 12:25:08 +0200

    Add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -3,7 +3,7 @@ const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
 
-var userSchema = new mongoose.Schema({
+var UserSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
@@ -36,14 +36,14 @@ var userSchema = new mongoose.Schema({
 
 //overwrite the body response that sent back to the user. 
 //This way the user won't get the password or the token back.
-userSchema.methods.toJSON = function () {
+UserSchema.methods.toJSON = function () {
     var user = this;
     var userObject = user.toObject();
 
     return _.pick(userObject, ['_id', 'email']);
 };
 
-userSchema.methods.generateAuthToken = function () {
+UserSchema.methods.generateAuthToken = function () { //.methods -> instance method
     var user = this;
     var access = 'auth';
     var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
@@ -55,6 +55,26 @@ userSchema.methods.generateAuthToken = function () {
     });
 };
 
-var User = mongoose.model('User', userSchema);
+UserSchema.statics.findByToken = function (token) {  //.statics -> model method
+    var User = this; 
+    var decoded;
+
+    try {
+        decoded = jwt.verify(token, 'abc123');
+    } catch (e) {
+        return Promise.reject('this meassage will be the err argument of catch handler at server.js');
+        // return new Promise((resolve, reject) => {
+        //     reject();
+        // });
+    }
+
+    return User.findOne({
+        '_id': decoded._id,
+        'tokens.token': token, //need to wrap in quotes when using .
+        'tokens.access': 'auth'
+    });
+};
+
+var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
